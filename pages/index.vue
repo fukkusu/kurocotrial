3d6aa040b6c1bc4e49ac156b03847efd530d3432
Fukkusu <149557634+fukkusu@users.noreply.github.com>
2024-05-09 10:02:27 +0900

    first page

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
 <script>
 export default {
   async asyncData({ $axios }) {
-    return { response: await $axios.$get('/rcms-api/3/service/3') };
+    return { response: await $axios.$get('/rcms-api/3/service/7') };
   },
 };
 </script>
